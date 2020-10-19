58e3687cb38c44ebeb9185957fdb3732779dc05e
Juan Jimenez <juanjimeneztj@gmail.com>
2020-10-19 12:53:10 -0700

    removed the same day Cookie

--- gsweb.juanjimeneztj.main-page.js
+++ gsweb.juanjimeneztj.main-page.js
@@ -1,17 +1,11 @@
 $(document).ready(function(){
     let gsweb_ck = Cookies.get('gsweb_visit_week');
-    let gsweb_cktd = Cookies.get('gsweb_visit_week_td');
     
     if(gsweb_ck){
-        if(gsweb_cktd){
-            console.log('Already registrated');
-        }else{
-            GSWEB_hideModal();
-        }
+        GSWEB_hideModal();
     }else{
         GSWEB_init();
     }
-    // Cookies.remove('gsweb_visit_week');
 });
 
 function GSWEB_hideModal(){
@@ -22,5 +16,4 @@ function GSWEB_hideModal(){
 
 function GSWEB_init(){
     Cookies.set('gsweb_visit_week','1',{expires: Math.random() * (7 - 1) + 1 });
-    Cookies.set('gsweb_visit_week_td','1',{expires: 1 });
 }
